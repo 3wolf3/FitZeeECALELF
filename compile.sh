dc716f82e51ac6b73ba603511a8bc6d6a410b6a4
Hengne <Hengne.Li@CERN.CH>
2014-05-21 09:17:46 +0200

    add fitzee.cc

--- compile.sh
+++ compile.sh
@@ -9,3 +9,13 @@ g++ -o fitzeescale.exe fitzeescale.cc \
       -lMathCore -lThread -lMinuit -lMinuit2 -lpthread \
       -lTreePlayer \
       -Wl,-rpath,${ROOTSYS}/lib -lm -ldl
+
+g++ -o fitzee.exe fitzee.cc \
+      -pthread -m64 \
+      -I${ROOTSYS}/include \
+      -I./  -L${ROOTSYS}/lib \
+      -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d \
+      -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics \
+      -lMathCore -lThread -lMinuit -lMinuit2 -lpthread \
+      -lTreePlayer \
+      -Wl,-rpath,${ROOTSYS}/lib -lm -ldl
