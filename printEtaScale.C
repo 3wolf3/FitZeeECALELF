b87b7bffd40ccb64de14fc646206796438caef35
Hengne Li <Hengne.Li@CERN.CH>
2014-05-26 12:59:22 +0200

    print etascale from the eta-scale fit output root file

---
+++ printEtaScale.C
@@ -0,0 +1,25 @@
+{
+//TFile* file = new TFile("etascaleratio_ABC_inv.root");
+TFile* file = new TFile("fitzeescale_out_ABC_data.root");
+
+//TH1D* h1 = (TH1D*)file->Get("hEtaScaleRatio");
+TH1D* h1 = (TH1D*)file->Get("hEtaScale");
+
+
+int EtaRing = -124;
+
+for (int i=1; i<=h1->GetNbinsX(); i++)
+{
+  std::cout << EtaRing << " " << h1->GetBinContent(i) << " " << h1->GetBinError(i) << std::endl;
+
+  if (EtaRing==-1)
+  {
+    EtaRing += 2;
+  }
+  else 
+  {
+    EtaRing += 1 ;
+  }
+}
+
+}
