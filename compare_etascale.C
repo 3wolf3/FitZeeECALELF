55556d2b60c1385b54e7c3d7d74535871a8f51f3
Hengne Li <Hengne.Li@CERN.CH>
2014-05-29 20:25:59 +0200

    minor change of compare eta scale macro

--- compare_etascale.C
+++ compare_etascale.C
@@ -1,15 +1,20 @@
 {
 
-TFile* file1 = new TFile("etascale_calculateEtaScaleFromICs_ABC_data_EEP.root");
-TFile* file2 = new TFile("etascale_calculateEtaScaleFromICs_ABC_data_ExceptEEP_EEP.root");
-//TFile* file2 = new TFile("fitzeescale_out_ABC_data.root");
-//TFile* file1 = new TFile("etascale_calculateEtaScaleFromICs_ABC_inv_ExceptEEP_EEP.root");
+TFile* file1 = new TFile(
+"etascale_calculateEtaScaleFromICs_ABC_data_EEP.root"
+//"etascale_calculateEtaScaleFromICs_ABC_inv_ExceptEEP_EEP.root"
+);
 
-//TFile* file2 = new TFile("etascale_calculateEtaScaleFromICs_ABC_empty_EEP.root");
-//TFile* file2 = new TFile("etascaleratio_ABC_inv.root");
+TFile* file2 = new TFile(
+"out_V8Elec_mode781_method7_evenodd0_Data_3/fitzeescale_out_V8Elec_Mode781_Method7_ABC_Data_3.root"
+//"etascale_calculateEtaScaleFromICs_ABC_data_ExceptEEP_EEP.root"
+//"fitzeescale_out_ABC_data.root"
+//"etascale_calculateEtaScaleFromICs_ABC_empty_EEP.root"
+//"etascaleratio_ABC_inv.root"
+);
 
-TH1D* h1 = (TH1D*)file1->Get("hEtaScaleRatio");
-TH1D* h2 = (TH1D*)file2->Get("hEtaScaleRatio");
+//TH1D* h1 = (TH1D*)file1->Get("hEtaScaleRatio");
+//TH1D* h2 = (TH1D*)file2->Get("hEtaScaleRatio");
 //TH1D* h2 = (TH1D*)file2->Get("hEtaScale");
 
 h1->SetLineColor(2);
