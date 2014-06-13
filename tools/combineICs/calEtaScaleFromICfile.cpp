0d5b0a584099020cd60373969b63ef375214bfb5
Hengne Li <Hengne.Li@CERN.CH>
2014-06-13 17:19:05 +0200

    tiny change of help message

--- tools/combineICs/calEtaScaleFromICfile.cpp
+++ tools/combineICs/calEtaScaleFromICfile.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
 {
   if( argc<2 )
   {
-    std::cout << argv[0] << "input_ICFile" << std::endl;
+    std::cout << argv[0] << " input_ICFile" << std::endl;
     return 0;
   }
 
