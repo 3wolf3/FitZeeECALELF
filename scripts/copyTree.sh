301d7b7a2a9c5cee418f5713afa8cb48becc7003
Hengne <Hengne.Li@CERN.CH>
2014-06-24 14:34:48 +0200

    copyTree.sh

--- scripts/copyTree.sh
+++ scripts/copyTree.sh
@@ -3,15 +3,55 @@
 tag="EEN"
 sele="etaSCEle[1]<-2.2&&recoFlagsEle[1]==-1"
 
-#infile="root://eoscms//eos/cms/store/group/alca_ecalcalib/ecalelf/heli/ntuple/NtupleComb_DoubleElectron-ZSkim-RUN2012B-22Jan-v1.root"
-infile="NtupleComb_DoubleElectron-ZSkim-RUN2012B-22Jan-v1.root"
-#outfile="root://eoscms//eos/cms/store/group/alca_ecalcalib/ecalelf/heli/ntuple/NtupleComb_DoubleElectron-ZSkim-RUN2012B-22Jan-v1_${tag}.root"
-outfile="NtupleComb_DoubleElectron-ZSkim-RUN2012B-22Jan-v1_${tag}.root"
-
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012A-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012A-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
 date
 
-copyTree.exe $infile $outfile -1 $sele
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012B-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012B-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
+date
 
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012C-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012C-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
+date
+
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012D-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012D-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
+date
+
+tag="EEP"
+sele="etaSCEle[1]>2.2&&recoFlagsEle[1]==-1"
+
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012A-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012A-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
+date
+
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012B-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012B-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
+date
+
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012C-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012C-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
+date
+
+date
+infile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012D-22Jan-v1.root"
+outfile="NtupleComb_DoubleElectron-ShervinZSkimPath-RUN2012D-22Jan-v1_${tag}.root"
+copyTree.exe $infile $outfile -1 $sele &> ${outfile}.log &
 date
 
 
