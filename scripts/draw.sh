3e8a117ca85f14022c5e3e6ec8144ef873714f01
Hengne <Hengne.Li@CERN.CH>
2014-06-24 16:00:30 +0200

    draw.sh

--- scripts/draw.sh
+++ scripts/draw.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-drawMee.exe config/drawMee.config.data_ZSkim_2012AB_EEN
-drawMee.exe config/drawMee.config.data_ZSkim_2012AB_EEP
-drawMee.exe config/drawMee.config.data_ZSkim_2012C_EEN
-drawMee.exe config/drawMee.config.data_ZSkim_2012C_EEP
-drawMee.exe config/drawMee.config.data_ZSkim_2012D_EEN
-drawMee.exe config/drawMee.config.data_ZSkim_2012D_EEP
+drawMee.exe config/drawMee.config.data_ShervinZSkimPath_2012AB_EEN
+drawMee.exe config/drawMee.config.data_ShervinZSkimPath_2012AB_EEP
+drawMee.exe config/drawMee.config.data_ShervinZSkimPath_2012C_EEN
+drawMee.exe config/drawMee.config.data_ShervinZSkimPath_2012C_EEP
+drawMee.exe config/drawMee.config.data_ShervinZSkimPath_2012D_EEN
+drawMee.exe config/drawMee.config.data_ShervinZSkimPath_2012D_EEP
+
+
+#drawMee.exe config/drawMee.config.data_ZSkim_2012AB_EEN
+#drawMee.exe config/drawMee.config.data_ZSkim_2012AB_EEP
+#drawMee.exe config/drawMee.config.data_ZSkim_2012C_EEN
+#drawMee.exe config/drawMee.config.data_ZSkim_2012C_EEP
+#drawMee.exe config/drawMee.config.data_ZSkim_2012D_EEN
+#drawMee.exe config/drawMee.config.data_ZSkim_2012D_EEP
 
 
 
